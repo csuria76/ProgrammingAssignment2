cbb21f42aab17341ba6c8743618260a65294662c
Carles Surià Albà <csuria76@gmail.com>
2014-05-21 09:41:31 +0200

    Make some corrections to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## This R file contains two functions that allow us to cache inverse matrix calculation
+## This R script file contains two functions that allow us to cache inverse matrix calculation
 ## The first funtion makeCacheMatrix creates a "special matrix" (a list) with the functions get(), set() (to get and set the matrix) 
 ## and getinv() and setinv() to cache and recover the inverse matrix.
 ## The second function cacheSolve takes the "special matrix" (the list created with the first function)
@@ -7,8 +7,8 @@
 ## The function makeCacheMatrix 
   ## First we initialize the cached inverse to NULL
   ## We define set() function to set the matrix and initialize cached inverse to NULL 
-  ## We define get() return the matrix
-  ## We define setinv() to chache inverse matrix
+  ## We define get() to return the matrix
+  ## We define setinv() to chache the inverse matrix
   ## We define getinv() to return the chached inverse matrix
   ## Finally we return a four objects list with functions get() and set() (to get and set the matrix) and functions setinv() and getinv() 
   ## (to get and set inverse)
@@ -29,10 +29,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## The function cacheSolve:
   ## First gets the chached value
-  ## If that cached value is not null prints a message and returns the inverse matrix then quits the function
-  ## If it did not return gets the matrix
-  ## Calculates inverse matrix using solve() 
-  ## Caches the inverse matrix
+  ## If that cached value is not NULL prints a message and returns the inverse matrix then quits the function
+  ## If it did not return gets the matrix using x$get()
+  ## Calculates the inverse matrix using solve() 
+  ## Caches the inverse matrix using x$setinv(inv)
   ## and prints (and returns) it
 cacheSolve <- function(x, ...) {
   inv <- x$getinv()
